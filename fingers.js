4ebd02c5adb55d9470f9fe010704271b2182fa25
Dmitry Shkulnik <demonarxs1@yandex.ru>
2023-09-20 02:56:37 +0800

    fix(fingers): fix mistakes

--- fingers.js
+++ fingers.js
@@ -1,10 +1,12 @@
 const fingersEl = document.querySelector('#fingers');
+const fingerDesc = document.querySelector('#fingerDesc');
 const firstFingersStateEl = document.querySelector('#fingersState1');
 const secondFingersStateEl = document.querySelector('#fingersState2');
 
 fingersEl.addEventListener('touchmove', event => {
+    fingerDesc.textContent = JSON.stringify(event);
     if (event.changedTouches.length === 2) {
-        firstFingersStateEl.textContent = JSON.parse(event.changedTouches[0])
-        secondFingersStateEl.textContent = JSON.parse(event.changedTouches[1])
+        firstFingersStateEl.textContent = JSON.stringify(event.changedTouches[0]);
+        secondFingersStateEl.textContent = JSON.stringify(event.changedTouches[1]);
     }
 })
