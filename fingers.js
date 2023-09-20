45ad4f0499f78dece9774e11f06815b33a01c8e3
Dmitry Shkulnik <demonarxs1@yandex.ru>
2023-09-20 09:49:34 +0800

    feat(fingers): add meta

--- fingers.js
+++ fingers.js
@@ -1,12 +1,12 @@
 const fingersEl = document.querySelector('#fingers');
-const fingerDesc = document.querySelector('#fingerDesc');
 const firstFingersStateEl = document.querySelector('#fingersState1');
 const secondFingersStateEl = document.querySelector('#fingersState2');
 
 fingersEl.addEventListener('touchmove', event => {
-    fingerDesc.textContent = JSON.stringify(event);
     if (event.changedTouches.length === 2) {
-        firstFingersStateEl.textContent = JSON.stringify(event.changedTouches[0]);
-        secondFingersStateEl.textContent = JSON.stringify(event.changedTouches[1]);
+        const first = event.changedTouches[0];
+        const second = event.changedTouches[1];
+        firstFingersStateEl.textContent = `force: ${first.force}, radiusX: ${first.radiusX}, radiusY: ${first.radiusY}, screenX: ${first.screenX}, screenY: ${first.screenY}`;
+        secondFingersStateEl.textContent = `force: ${second.force}, radiusX: ${second.radiusX}, radiusY: ${second.radiusY}, screenX: ${second.screenX}, screenY: ${second.screenY}`;
     }
 })
