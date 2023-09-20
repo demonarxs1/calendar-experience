b942aff0064ea82a8b1ad16f82d7bba3f8a3477d
Dmitry Shkulnik <demonarxs1@yandex.ru>
2023-09-20 09:57:46 +0800

    fix(fingers): mistakes

--- fingers.js
+++ fingers.js
@@ -1,12 +1,6 @@
 const fingersEl = document.querySelector('#fingers');
-const firstFingersStateEl = document.querySelector('#fingersState1');
-const secondFingersStateEl = document.querySelector('#fingersState2');
+const fingersStateEl = document.querySelector('#fingersState');
 
 fingersEl.addEventListener('touchmove', event => {
-    if (event.changedTouches.length === 2) {
-        const first = event.changedTouches[0];
-        const second = event.changedTouches[1];
-        firstFingersStateEl.textContent = `force: ${first.force}, radiusX: ${first.radiusX}, radiusY: ${first.radiusY}, screenX: ${first.screenX}, screenY: ${first.screenY}`;
-        secondFingersStateEl.textContent = `force: ${second.force}, radiusX: ${second.radiusX}, radiusY: ${second.radiusY}, screenX: ${second.screenX}, screenY: ${second.screenY}`;
-    }
+    fingersStateEl.innerHTML = Array.from(event.changedTouches).map(it => `Id: ${it.identifier}, force: ${it.force}, radiusX: ${it.radiusX}, radiusY: ${it.radiusY}, screenX: ${it.screenX}, screenY: ${it.screenY})`).join('; ');
 })
